1e0cf3f832413b0b4f5823937faeaac6417e16ab
BonnevieAmel <amel.bonnevie@gmail.com>
2024-05-28 12:40:45 +0200

    Harmonia 05 done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,20 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE TABLE category (
+  id INT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE program (
+  id INT PRIMARY KEY auto_increment not null,
+  title VARCHAR(255) NOT NULL,
+  synopsys TEXT NOT NULL,
+  poster TEXT,
+  country VARCHAR(100) NOT NULL,
+  year INT NOT NULL,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
